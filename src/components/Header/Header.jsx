1584ccde328f674cb73364d479142e0228003933
´Yudith´ <yudi@geeksacademy.es>
2024-05-29 18:13:34 +0200

    Login y registrar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { useDispatch, useSelector } from "react-redux";
+import { userData, userLogout } from '../../app/slices/userSlice';
+import "./Header.css";
+
+export const Header = () => {
+    const userLogued = useSelector(userData).decodificado;
+    const userToken = useSelector(userData).token;
+    const [isLogued, setLogued] = useState(false);
+    const dispatch = useDispatch();
+
+    // Es un evento que comprueba si estás logueado
+    useEffect(() => {
+        if (userToken && userToken.length > 5) {
+            setLogued(true);
+        } else {
+            setLogued(false);
+        }
+    }, [userToken]);
+
+    // Elimino el token y el usuario guardado en redux
+    const logout = () => {
+        dispatch(userLogout());
+    };
+
+    return (
+        <Navbar collapseOnSelect expand="lg" className="headerDesing">
+            <Container>
+                <Navbar.Brand>
+                    <p className='p_color'>
+                        {userLogued && userLogued.firstName ? (
+                            <>{"Bienvenido " + userLogued.firstName.toUpperCase()}</>
+                        ) : (
+                            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+                        )}
+                    </p>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto nav_link_home">
+                        {isLogued && (
+                            <>
+                                <Nav.Link as={Link} to="/"><p className='p_color'>Inicio</p></Nav.Link>
+                                <Nav.Link as={Link} to="/profile"><p className='p_color'>Perfil</p></Nav.Link>
+                                {userLogued && userLogued.userRole === "1" ? (
+                                    <Nav.Link as={Link} to="/admin"><p className='p_color'>Admin</p></Nav.Link>
+                                ) : (
+                                    <Nav.Link as={Link} to="/citas"><p className='p_color'>Citas</p></Nav.Link>
+                                )}
+                            </>
+                        )}
+                    </Nav>
+                    {!isLogued && (
+                        <Nav className="me-auto nav_link_log">
+                            <>
+                                <Nav.Link as={Link} to="/login"><p className='p_color'>Acceso</p></Nav.Link>
+                                <Nav.Link as={Link} to="/create"><p className='p_color'>Registro</p></Nav.Link>
+                            </>
+                        </Nav>
+                    )}
+                    {isLogued && (
+                        <Nav.Link onClick={logout}><p className='p_color'>Cerrar Sesión</p></Nav.Link>
+                    )}
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+
+
---
+++ src/pages/Body/Body.jsx
@@ -0,0 +1,25 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Login } from "../Login/Login";
+import { Home } from "../Home/Home";
+import { Register } from "../Register/Register";
+import { Profile } from "../Profile/Profile";
+import { Appointments } from "../Appointment/Appointment";
+import { Admin } from "../Admin/Admin";
+
+
+export const Body = () => {
+
+  return (
+    <>
+      <Routes>
+        <Route path="*" element={<Navigate to="/" />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/create" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/citas" element={< Appointments/>}/>
+        <Route path="/admin" element={<Admin/>}/>
+      </Routes>
+    </>
+  );
+};
